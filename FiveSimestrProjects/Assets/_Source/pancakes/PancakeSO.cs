613a5d4caa5aaf4d5a650faa4fb7ffc981374149
ikvelibrium <stalker.alex.perel@mail.ru>
2022-12-26 00:00:59 +0300

    Initial

---
+++ FiveSimestrProjects/Assets/_Source/pancakes/PancakeSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PancakeSO : ScriptableObject
+{
+    [SerializeField] private GameObject _pancakePref;
+    [SerializeField] private int _points;
+
+}
